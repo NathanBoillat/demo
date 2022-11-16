542c39c82f6d8abe6a026de00801e62ebf9b0723
NathanBoillat <e232676@edu.sbl.ch>
2022-11-16 14:49:13 +0100

    to green

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function handleKeyPress (ev) {
-    console.log(ev)
+function handleKeyPress(ev) {
+    if (ev.code === "Space") {
+console.log(ev)
+    const player = document.querySelector("#player")
+    player.setAttribute("cx", 100 * Math.random())
+    player.setAttribute("cy", 100 * Math.random())
+    } else if (ev.key === "r") {
+        player.setAttribute("fill", "red")
+    } else if (ev.key === "g") {
+        player.setAttribute ("fill", "red")
+    }
 }
 
 
