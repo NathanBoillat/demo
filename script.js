879b1df43becd10a99c14fa6b7aedfcdee265a58
NathanBoillat <e232676@edu.sbl.ch>
2022-11-18 08:40:40 +0100

    left right

--- script.js
+++ script.js
@@ -6,10 +6,15 @@ function handleKeyPress(ev) {
         player.setAttribute("fill", "red");
     } else if (ev.key === "g") {
         player.setAttribute ("fill", "green");
+    } else if (ev.code === "ArrowRight") {
+        dx = 1   
+    } else if (ev.code === "ArrowLeft") {
+        dx = -1   
     }
 }
 
 let dy = 0
+let dx = 0
  
 function gameLoop() {
 // Update game state  
@@ -17,14 +22,17 @@ dy = Math.min(dy + 0.05, 1)
 
 
     const player = document.querySelector("#player");
+    
+    
     let posY = parseFloat(player.getAttribute("cy"));
     let r = parseInt(player.getAttribute("r"))
     if (posY > 100 - r - dy) {
         dy = 0 
     }
-
-
     player.setAttribute("cy", posY + dy);
+
+    let posX = parseFloat(player.getAttribute("cx"));
+    player.setAttribute("cx", posX + dx);
 
 
     //Loop forever
