b0186535500dd1a9e66ce58b23ee55e0a2e49357
NathanBoillat <e232676@edu.sbl.ch>
2022-11-18 09:06:11 +0100

    added items

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ console.log(ev)
     const player = document.querySelector("#player")
     player.setAttribute("cx", 100 * Math.random())
     player.setAttribute("cy", 100 * Math.random())
+    spawnItem()
     } else if (ev.key === "r") {
         player.setAttribute("fill", "red")
     } else if (ev.key === "g") {
-        player.setAttribute ("fill", "red")
+        player.setAttribute ("fill", "green")
     }
 }
 
+
+function spawnItem() {
+    const NS = "http://www.w3.org/2000/svg"
+    const item = document.createElementNS(NS, "circle")
+    item.setAttribute("cx", 100 * Math.random())
+    item.setAttribute("cy", 20 * Math.random() + 80)
+    item.setAttribute("r", 2)
+    item.setAttribute("fill", "black") 
+    document.querySelector("svg").appendChild(item)  
+}
 
 window.onkeydown = handleKeyPress
