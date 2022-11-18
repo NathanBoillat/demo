cb89c5d2e35e3a25b48409c2d3d01dc70085f4de
NathanBoillat <e232676@edu.sbl.ch>
2022-11-18 08:17:44 +0100

    Gravitation

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 function handleKeyPress(ev) {
+    console.log(ev)      
     if (ev.code === "Space") {
-console.log(ev)
     const player = document.querySelector("#player")
     player.setAttribute("cx", 100 * Math.random())
     player.setAttribute("cy", 100 * Math.random())
     } else if (ev.key === "r") {
-        player.setAttribute("fill", "red")
+        player.setAttribute("fill", "red");
     } else if (ev.key === "g") {
-        player.setAttribute ("fill", "red")
+        player.setAttribute ("fill", "green");
     }
 }
 
+function gameLoop() {
+    const player = document.querySelector("#player");
+    let posY = parseInt(player.getAttribute("cy"));
+    player.setAttribute("cy", posY + 1);
 
-window.onkeydown = handleKeyPress
+
+    //Loop forever
+    window.requestAnimationFrame(gameLoop);
+}
+
+gameLoop()    
+
+window.onkeydown = handleKeyPress;
